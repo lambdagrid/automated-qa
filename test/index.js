0bf6b50f2c4cc7c6ebb469df8051f99c88f662ea
Dillon Forrest <dillonforrest@gmail.com>
2018-11-03 11:52:53 -0700

    less trivial test

--- test/index.js
+++ test/index.js
@@ -1,3 +1,13 @@
-1 + 1;
-console.log('starting tests...');
-console.log('tests finished!');
+const request = require('request-promise');
+
+const url = (path) => `${process.env.ROOT}${path}`;
+
+async function runTests() {
+  console.log('starting tests...');
+  const result1 = await request(url('/spotify.com'));
+  const result2 = await request(url('/spotify.com'));
+  console.log('assert true', result1 == result2);
+  console.log('finished tests!');
+}
+
+runTests();
